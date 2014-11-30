b3245bb8ed77a1d3d26f1272d05cda7dc70e1117
Mads Christensen <mads0307@gmail.com>
2014-11-30 18:10:01 +0100

    made visual bell

--- Clients/Approximator/ApproximatorClient/MainWindow.xaml.cs
+++ Clients/Approximator/ApproximatorClient/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using ApproximatorClient.Sensors;
 using WebSocketSharp;
+using System.Threading;
+using System.Windows.Media;
 
 namespace ApproximatorClient
 {
@@ -22,6 +24,40 @@ namespace ApproximatorClient
             new MouseSensor(userName);
             new KeyboardSensor(userName);
             new FaceDetectionSensor(userName, cameraIndex: 0);
+            Thread visualBellThread = new Thread(visualBell);
+            visualBellThread.IsBackground = true;
+            visualBellThread.Start();
+        }
+
+        public void visualBell()
+        {
+            while (true)
+            {
+                try
+                {
+                    this.Dispatcher.Invoke(new Action(() => this.Top = 0));
+                    this.Dispatcher.Invoke(new Action(() => this.Left = System.Windows.SystemParameters.PrimaryScreenWidth - this.Width));
+                    this.Dispatcher.Invoke(new Action(() => this.Topmost = true));
+                    
+                    for (int i = 0; i < 50; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            this.Dispatcher.Invoke(new Action(() => indicator.Fill = new SolidColorBrush(Colors.Black)));
+                        }
+                        else
+                        {
+                            this.Dispatcher.Invoke(new Action(() => indicator.Fill = new SolidColorBrush(Colors.White)));
+                        }
+                        Thread.Sleep(400);
+                    }
+                    this.Dispatcher.Invoke(new Action(() => this.Topmost = false));
+                    Thread.Sleep(60 * 60 * 30); //wait for 30 minutes
+                }
+                catch (Exception ex)
+                {
+                }
+            }
         }
     }
 }
